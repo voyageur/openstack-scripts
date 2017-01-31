23e7cee9a5046ddf3d5865cb60fe2ca3765862f2
Bernard Cafarelli <bcafarel@redhat.com>
2017-01-31 11:29:00 +0100

    Update to OSC commands

--- simple_vms.sh
+++ simple_vms.sh
@@ -5,14 +5,14 @@
 
 for inst in cirros1 cirros2
 do
-    if ! nova show "${inst}" > /dev/null 2>&1
+    if ! openstack server show "${inst}" > /dev/null 2>&1
     then
-        nova boot --image "${IMAGE}" --flavor "${FLAVOR}" \
-            --key-name "${SSH_KEYNAME}" --security-groups "${SECGROUP}" \
+        openstack server create --image "${IMAGE}" --flavor "${FLAVOR}" \
+            --key-name "${SSH_KEYNAME}" --security-group "${SECGROUP}" \
             "${inst}"
 
-        floating=$(openstack ip floating create public -f value -c floating_ip_address)
-        openstack ip floating add "${floating}" "${inst}"
+        floating=$(openstack floating ip create public -f value -c floating_ip_address)
+        openstack server add floating ip "${inst}" "${floating}"
     fi
 done
 
@@ -21,7 +21,7 @@ sleep 5
 route_to_subnetpool
 
 # Basic web server
-for fixed_ip in $(openstack ip floating list -f value -c "Fixed IP Address")
+for fixed_ip in $(openstack floating ip list -f value -c "Fixed IP Address" | grep -v None)
 do
     ssh cirros@${fixed_ip} 'while true; do echo -e "HTTP/1.0 200 OK\r\n\r\nWelcome to $(hostname)" | sudo nc -l -p 80 ; done&'
 done
